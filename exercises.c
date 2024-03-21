6b55cabf110867a4608e322f3ab150ce8a5af422
Your Name <you@example.com>
2024-03-21 23:35:05 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -112,7 +112,7 @@ int parentesisBalanceados(char *cadena) {
   for(int i = 0; cadena[i] != '\0'; i++)
     {
       if(cadena[i] == '(' || cadena[i] == '{' || cadena[i] == '[')
-        push(estak. cadena[i]); //inserta elemento E en pila P
+        push(estak, *cadena[i]); //inserta elemento E en pila P
     }
   return 0;
 }
